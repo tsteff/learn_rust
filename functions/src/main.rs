a91b3f715e86eb5bbe4a832885348270e38cd3b9
Timothy Steff <timothy.steff@rakuten.com>
2019-12-27 19:06:44 +0100

    expression example

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
-    another_function(5);
-    another_function_two(5, 6);
-}
+    let x = 5;
 
-fn another_function(x : i32) {
-    println!("The value of x is: {}", x);
-}
+    let y = {
+        let x = 3;
+        x + 1
+    };
 
-fn another_function_two(x: i32, y: i32) {
-    println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
 }
