cc90f441503117ac2be6b972b22b10452565fdc0
Timothy Steff <timothy.steff@rakuten.com>
2019-12-27 19:42:18 +0100

    Add while loop type

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
     let mut counter = 0;
-
     let result = loop {
         counter += 1;
 
@@ -8,6 +7,14 @@ fn main() {
             break counter * 2;
         }
     };
-
     println!("The result is {}", result);
+
+
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
 }
