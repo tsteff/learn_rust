88be5d58507fc0d3d773d50f9cb2a94d7f558e1a
Timothy Steff <timothy.steff@rakuten.com>
2019-12-27 19:41:05 +0100

    Add loop with return using break

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let condition = true;
-    let number = if condition {
-        5
-    } else {
-        6
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
     };
 
-    println!("The value of number is: {}", number);}
+    println!("The result is {}", result);
+}
