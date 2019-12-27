635feab5a907524c4d2ceadf4a6bffb636e3ca55
Timothy Steff <timothy.steff@rakuten.com>
2019-12-27 19:23:28 +0100

    return example

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let x = 5;
+    let x = plus_one(5);
 
-    let y = {
-        let x = 3;
-        x + 1
-    };
+    println!("The value of x is: {}", x);
+}
 
-    println!("The value of y is: {}", y);
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
