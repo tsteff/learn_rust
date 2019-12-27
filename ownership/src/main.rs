2a71a4fcb4ab5f7872f0a7bfb340a461809ae337
Timothy Steff <timothy.steff@rakuten.com>
2019-12-28 00:15:03 +0100

    That string literals are slices

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -30,9 +30,20 @@ fn main() {
 //    s.clear(); we couldn't do this as word is still just a reference to s and we are trying
     // to use it below
     println!("word: {}", word);
+
+
+    let my_string = String::from("hello world");
+    // first_word works on slices of `String`s
+    let word = first_word(&my_string[..]);
+    let my_string_literal = "hello world";
+    // first_word works on slices of string literals
+    let word = first_word(&my_string_literal[..]);
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
