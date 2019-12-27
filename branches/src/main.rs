ea20f18aaeae6a4c3fbcf9dfb377e600abcc1071
Timothy Steff <timothy.steff@rakuten.com>
2019-12-27 19:45:06 +0100

    loop collection

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -17,4 +17,10 @@ fn main() {
         number -= 1;
     }
     println!("LIFTOFF!!!");
+
+
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
 }
