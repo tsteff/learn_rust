a6da6761f036a4934ce99d158defb1e88572e6db
Timothy Steff <timothy.steff@rakuten.com>
2019-12-28 19:42:27 +0100

    Starting structs

---
+++ structs/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    user1.email = String::from("anotheremail@example.com");
+
+
+    let userMe = build_user(String::from("test@examle.com"), String::from("test"));
+
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count,
+    };
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1 // this is the same as the above but will fill in the remaining fields automatically
+    };
+
+
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+//        email: email, not required as the variables and field names are the same
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
