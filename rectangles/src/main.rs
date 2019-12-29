41fad933261e1c2e31bb8cedbf8a8affea5723e8
Timothy Steff <timothy.steff@rakuten.com>
2019-12-29 12:28:32 +0100

    Allow printing of structs

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,12 +1,10 @@
 fn main() {
-    let rectangle = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
-        height: 50
+        height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.", area(&rectangle)
-    );
+    println!("rect1 is {:#?}", rect1); // the hash makes prettyprint
 }
 
 // use & as we don't want to change ownership
@@ -14,6 +12,7 @@ fn area(rectangle: &Rectangle) -> u32 {
     rectangle.height * rectangle.width
 }
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
